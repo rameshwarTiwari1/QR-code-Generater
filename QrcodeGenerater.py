9e4ebd18177c4aaa63861d8819809d89ba9340b7
Rameshwar Tiwari <91799011+rameshwarTiwari1@users.noreply.github.com>
2021-12-16 23:24:47 +0530

    Update QrcodeGenerater.py

--- QrcodeGenerater.py
+++ QrcodeGenerater.py
@@ -4,8 +4,8 @@ import png
 from pyqrcode import QRCode
 
 # String which represents the QR code
-s = "www.geeksforgeeks.org"
-
+s = "www.google.com"      # you can write anything here, which you wamt to generate QRcode
+    
 # Generate QR code
 url = pyqrcode.create(s)
 
@@ -13,4 +13,4 @@ url = pyqrcode.create(s)
 url.svg("myqr.svg", scale=8)
 
 # Create and save the png file naming "myqr.png"
-url.png('myqr.png', scale=6)
+url.png('myqr.png', scale=6)  #myqr.png is file name 
